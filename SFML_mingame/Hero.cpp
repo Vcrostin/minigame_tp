3201b303ef7acb7795be961bc0a642303cb384b9
Широкин Костя <kostyash2001@mail.ru>
2020-12-20 18:11:19 +0300

    hero cpp add

--- SFML_mingame/Hero.cpp
+++ SFML_mingame/Hero.cpp
@@ -11,12 +11,14 @@ Hero::~Hero() { }
 void Hero::setTexture(std::string fname) {
 	if (texture.loadFromFile(fname)) {
 		sprite.setTexture(texture);
-	}	
+		std::cout << "Hero size: " << texture.getSize().x << ' ' << texture.getSize().y << '\n';
+	}
 }
 
 void Hero::setTexture(sf::Texture& text) {
 	texture = text;
 	sprite.setTexture(texture);
+	std::cout << "Hero size: " << texture.getSize().x << ' ' << texture.getSize().y << '\n';
 }
 
 void Hero::setPosition(sf::Vector2f a) {
@@ -29,4 +31,20 @@ void Hero::move(sf::Vector2f a) {
 
 sf::Sprite Hero::getSprite() {
 	return sprite;
+}
+
+bool Hero::checkBorder(sf::Vector2f& a, sf::VideoMode& b) {
+	if (sprite.getPosition().y + a.y < 40 || sprite.getPosition().x + a.x < 0
+		|| b.height - texture.getSize().y - a.y < sprite.getPosition().y
+		|| b.width < sprite.getPosition().x + texture.getSize().x + a.x) {
+		std::cout << "False\n";
+		return 0;
+	}
+
+	/*if (0 > sprite.getPosition().x + a.x || sprite.getPosition().x + a.x > b.width - sprite.getTexture()->getSize().x
+		|| sprite.getPosition().y + a.y < 40 || sprite.getPosition().x + a.y > b.height - sprite.getTexture()->getSize().y) {
+		std::cout << "False\n";
+		return 0;
+	}*/
+	return 1;
 }
