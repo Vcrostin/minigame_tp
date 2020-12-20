c141f23e19d23151b93f401df5327e86bbd24492
Широкин Костя <kostyash2001@mail.ru>
2020-12-20 17:23:16 +0300

    Create MFC project and added yosiky layer

---
+++ MFC_loader_SFML/MFC_loader_SFML.h
@@ -0,0 +1,32 @@
+﻿
+// MFC_loader_SFML.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMFCloaderSFMLApp:
+// Сведения о реализации этого класса: MFC_loader_SFML.cpp
+//
+
+class CMFCloaderSFMLApp : public CWinApp
+{
+public:
+	CMFCloaderSFMLApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCloaderSFMLApp theApp;
